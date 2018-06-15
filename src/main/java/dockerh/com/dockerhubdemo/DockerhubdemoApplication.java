d31d236cbf99c14a43c5c0f05ec9a2a9c16318d1
anilp02 <anilkumarp31@gmail.com>
2018-06-15 03:09:48 -0400

    init d

---
+++ src/main/java/dockerh/com/dockerhubdemo/DockerhubdemoApplication.java
@@ -0,0 +1,12 @@
+package dockerh.com.dockerhubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerhubdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerhubdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/dockerh/com/dockerhubdemo/DockerhubdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package dockerh.com.dockerhubdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerhubdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
